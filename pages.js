888304c812fd9e5dfe6d532268117f75018d86d9
navjotsinghmanshahia <107827342+navjotsinghmanshahia@users.noreply.github.com>
2022-06-20 17:17:54 +0530

    Add files via upload

---
+++ pages.js
@@ -0,0 +1,17 @@
+const ghpages = require("gh-pages");
+const pathname = `${__dirname}/build`;
+const repoURL = "https://github.com/navjotsinghmanshahia/PROFILIO";
+
+ghpages.publish(
+  pathname,
+  {
+    branch: "master",
+    repo: repoURL,
+  },
+  (err) => {
+    if (err) console.log("ERROR: ", err);
+    else console.log("PUBLISHED");
+  }
+);
+
+S
